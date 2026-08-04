f31d15efc9688b8446010f3573721d5d558f27f2
Umeshfarrow <umeshfarrow@gmail.com>
2026-08-04 04:24:07 +0000

    perf: self-host font-awesome to remove third-party cookie and external request

--- assets/js/async-css.js
+++ assets/js/async-css.js
@@ -1,7 +1,7 @@
 // async-css.js - load non-critical stylesheets (fonts, icon fonts) without blocking first paint
 const asyncStylesheets = [
   'assets/css/fonts.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'assets/css/fontawesome/all.min.css',
 ];
 
 asyncStylesheets.forEach(href => {
